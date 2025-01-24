6057dafe7f82b25e296fd770bbed296a5ec64ef1
Qullfie <lotarev200445@gmail.com>
2025-01-24 21:01:28 +0300

    made database connection problems handeling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  rescue_from ActiveRecord::ConnectionNotEstablished, with: :handle_database_connection_error
+  rescue_from PG::ConnectionBad, with: :handle_database_connection_error
+  rescue_from ActiveRecord::StatementInvalid, with: :handle_database_connection_error
+
+  private
+
+  def handle_database_connection_error(exception)
+    # Логирование ошибки (опционально)
+    Rails.logger.error "Database connection error: #{exception.message}"
+
+    # Отображение кастомного сообщения об ошибке
+    render "errors/database_error", status: :service_unavailable
+  end
 end
