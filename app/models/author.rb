4ec9e85156454116ad2cd4471f2ca6c6c8354c0b
Qullfie <lotarev200445@gmail.com>
2025-01-24 22:36:34 +0300

    uniqueness check implemented

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ApplicationRecord
     has_many :books, dependent: :destroy
-    validates :name, presence: true, length: { minimum: 2 }
+    validates :name, presence: true, length: { minimum: 2 }, uniqueness: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,13 @@ class Book < ApplicationRecord
   belongs_to :genre
   validates :title, presence: true
   validates :year, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validate :unique_title_and_author
+
+  private
+
+  def unique_title_and_author
+    if Book.exists?(title: title, author_id: author_id)
+            errors.add(:author, :taken)
+    end
+  end
 end
